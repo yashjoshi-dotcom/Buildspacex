bdd761a306222001730061e3ec03851b96b8f4f0
Simarjeet <sjthecoder@gmail.com>
2024-06-30 22:08:02 +0530

    Add: Navbar and its components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
-      <p>Hello World</p>
-      <Button size="lg">Hey</Button>
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
+			<Navbar />
+			<p>Hello World</p>
+			<Button size="lg">Hey</Button>
+		</main>
+	);
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,127 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+
+function Navbar() {
+	return (
+		<div className="fixed left-0 top-0 flex w-full items-center justify-center">
+			{/* Small screen */}
+			<div className="w-full md:hidden">
+				<div className="flex w-full shrink items-center justify-between p-6">
+					<h1 className="text-2xl font-semibold">
+						Brain<span className="text-primary">Fuzz</span>
+					</h1>
+					<Sheet>
+						<SheetTrigger>
+							<Menu />
+						</SheetTrigger>
+						<SheetContent side="top" className="h-full">
+							<SheetDescription className="flex h-full w-full flex-col items-center justify-center">
+								<nav className="flex grow items-center">
+									<ul className="flex flex-col gap-6 text-center">
+										<li>
+											<Link
+												className="font-semibold hover:underline hover:decoration-primary hover:underline-offset-4"
+												href="#"
+											>
+												Home
+											</Link>
+										</li>
+										<li>
+											<Link
+												className="font-semibold hover:underline hover:decoration-primary hover:underline-offset-4"
+												href="#"
+											>
+												Courses
+											</Link>
+										</li>
+										<li>
+											<Link
+												className="font-semibold hover:underline hover:decoration-primary hover:underline-offset-4"
+												href="#"
+											>
+												Testimonial
+											</Link>
+										</li>
+										<li>
+											<Link
+												className="font-semibold hover:underline hover:decoration-primary hover:underline-offset-4"
+												href="#"
+											>
+												Mentor
+											</Link>
+										</li>
+									</ul>
+								</nav>
+								<div className="flex shrink gap-4">
+									<Button size="sm" variant="outline">
+										Sign in
+									</Button>
+									<Button size="sm">Sign up</Button>
+								</div>
+							</SheetDescription>
+						</SheetContent>
+					</Sheet>
+				</div>
+			</div>
+			{/* Large Screen */}
+			<div className="hidden w-full max-w-7xl shrink items-center justify-between p-6 md:flex">
+				<h1 className="text-2xl font-semibold">
+					Brain<span className="text-primary">Fuzz</span>
+				</h1>
+				<nav>
+					<ul className="flex gap-6">
+						<li>
+							<Link
+								className="font-semibold hover:underline hover:decoration-primary hover:underline-offset-4"
+								href="#"
+							>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link
+								className="font-semibold hover:underline hover:decoration-primary hover:underline-offset-4"
+								href="#"
+							>
+								Courses
+							</Link>
+						</li>
+						<li>
+							<Link
+								className="font-semibold hover:underline hover:decoration-primary hover:underline-offset-4"
+								href="#"
+							>
+								Testimonial
+							</Link>
+						</li>
+						<li>
+							<Link
+								className="font-semibold hover:underline hover:decoration-primary hover:underline-offset-4"
+								href="#"
+							>
+								Mentor
+							</Link>
+						</li>
+					</ul>
+				</nav>
+				<div className="flex shrink gap-4">
+					<Button size="sm" variant="outline">
+						Sign in
+					</Button>
+					<Button size="sm">Sign up</Button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
